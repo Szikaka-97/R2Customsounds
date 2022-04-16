389c8b04f0b7c59a54fb14884905ab0fd46e8fab
Szikaka <michalmuszka@interia.pl>
2022-04-16 11:23:00 +0200

    Dodaj pliki projektów.

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace R2CustomSounds
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class CorePlugin : BaseUnityPlugin
+    {
+        public static CorePlugin instance;
+
+        private void Awake() {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            instance = this;
+
+            Harmony.CreateAndPatchAll(typeof(ModAudioManager));
+        }
+    }
+}
